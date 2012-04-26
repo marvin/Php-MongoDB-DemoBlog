69e8c5ff05cb0bf59931ccea88e26b31af4eb92f
davidnoelte <dnoelte@gmail.com>
2012-04-26 23:32:05 +0200

    Updated edit.php

--- edit.php
+++ edit.php
@@ -40,9 +40,14 @@ switch ($action) {
  					<p><input type="text" name="title" value="<?php echo $article['title']; ?>" id="title"></p>
  					<h3>Content</h3>
  					<p><textarea name="content" rows="20"><?php echo $article['content']; ?></textarea></p>
+ 					<input type="hidden" name="id" value="<?php echo $article['_id']; ?>" />
+ 					<p><input type="submit" name="btn_submit" value="Save"></p>
  				</form>
-
-
+ 			<?php else: ?>
+ 				<p>
+ 					Article saved. _id: <?php echo $id; ?>.
+ 					<a href="blog.php?id=<?php echo $id; ?>">Read it.</a>
+ 				</p>
  			<?php endif; ?>
  		</div><!-- / -->
  	</div><!-- / -->
